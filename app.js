a2bab78a43cd9bd2c3aadaa822c5822af590f0a5
qa921 <qa@agent51testing.com>
2026-09-14 20:58:07 +0530

    Seed multilingual simulation baseline

---
+++ app.js
@@ -0,0 +1,1 @@
+const supportedLanguages=['en','nl'];const supportedMarkets=["NL", "BE"];document.querySelector('#lang').addEventListener('change',function(){document.documentElement.lang=this.value;});
